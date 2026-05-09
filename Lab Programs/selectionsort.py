b75025da2fa6ba57685502e9e4dc62e95469a7d3
Sachin Saxena <42056722+sachin365123@users.noreply.github.com>
2026-05-09 20:53:34 +0530

    Add files via upload

---
+++ Lab Programs/selectionsort.py
@@ -0,0 +1,19 @@
+# Write a program for Selection sort.
+
+def selectionSort(arr):
+    
+    for i in range(len(arr)):
+        min_x = i
+
+        for item in range(i+1, len(arr)):
+            if arr[item] < arr[min_x]:
+                min_x = item
+
+        arr[i], arr[min_x] = arr[min_x], arr[i]
+
+
+
+arr = [20, 12, 10, 15, 2]
+selectionSort(arr)
+
+print(arr)
